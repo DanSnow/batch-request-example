b29e8e5f1599fa5124fff976d5b641e99c92d209
DanSnow <dododavid006@gmail.com>
2025-04-21 21:18:12 +0800

    chore: cleanup usused stuff in example

--- app/components/Devtools.tsx
+++ app/components/Devtools.tsx
@@ -1,26 +1,16 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useRouteContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { DevTools } from 'jotai-devtools'
 import { useEffect, useState } from 'react'
 
 function Devtools() {
-  const { queryClient, store } = useRouteContext({ from: '__root__' })
+  const { queryClient } = useRouteContext({ from: '__root__' })
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
     setMounted(true)
   }, [])
 
-  return (
-    mounted && (
-      <>
-        <ReactQueryDevtools client={queryClient} />
-        <DevTools store={store} />
-        <TanStackRouterDevtools position="bottom-right" />
-      </>
-    )
-  )
+  return mounted && <ReactQueryDevtools client={queryClient} />
 }
 
 Devtools.displayName = 'Devtools'
--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -13,10 +13,7 @@ export function NotFound({ children }: { children?: any }) {
         >
           Go back
         </button>
-        <Link
-          to="/"
-          className="bg-cyan-600 text-white px-2 py-1 rounded uppercase font-black text-sm"
-        >
+        <Link to="/" className="bg-cyan-600 text-white px-2 py-1 rounded uppercase font-black text-sm">
           Start Over
         </Link>
       </p>
